7b54fb6fe3678ff0567caec920c615c2b9f02634
dcl-jipay <dcl.jeanphig@18pixel.fr>
2019-01-12 15:33:17 +0100

    correction bugs

--- shifumi.js
+++ shifumi.js
@@ -1,80 +1,80 @@
-//Shi fu mi
-//pierre > ciseaux, papier > pierre, ciseaux > papier
-//atttribution du click au objet
-//
-//l'ordinateur choisi un objet
-//resultats
+
+ /*variables*/
+var pi = document.getElementById("r");/*pierre*/
+var pa = document.getElementById("p");/*papier*/
+var ci = document.getElementById("s");/*ciseau*/
 
 
-/*variables*/
-var pi = document.getElementById("pi");/*pierre*/
-var pa = document.getElementById("pa");/*papier*/
-var ci = document.getElementById("ci");/*ciseau*/
 
-
-/*fonction click choix du joueur*/
+/*fonction click sur icone + affichage de l'image dans la zone choix joueur*/
 function main (){
     pi.addEventListener("click", function () {
-        choixDuJoueur("pi");
+        game("pi");
         document.getElementById("txChoix").textContent = "la force de la pierre !";
+        document.getElementById("imgYou").src = "style/img/pierre.png";
     });
     
     pa.addEventListener("click", function () {
-        choixDuJoueur("pa");
+        game("pa");
         document.getElementById("txChoix").textContent = "la souplesse de la feuille !";
+        document.getElementById("imgYou").src = "style/img/feuille.png";
     });
     
     ci.addEventListener("click", function () {
-        choixDuJoueur("ci");
+        game("ci");
         document.getElementById("txChoix").textContent = "le tranchant du ciseau !";
+        document.getElementById("imgYou").src = "style/img/ciseaux.png";
     });
-    }
-main()
 
-
-/*fonction choix de l'ia*/
-function choixIa (){
-    var choix = ['pi', 'pa', 'ci'];
-    var randomNumber = Math.floor(Math.random() * 3)
-    return choix [randomNumber];
 }
 
-/*fonction comparaison entre le joueur et l'ia*/
-function choixDuJoueur(userchoise){
-    var choixDelIa = choixIa ();
-    if (userchoise == choixDelIa){
-        console.log(userchoise);
-        console.log(choixDelIa);
-        console.log("egalité");
-        document.getElementById("quiGagne").textContent = "Match nul !!";
-    } else if (userchoise == "pi" & choixDelIa == "ci"){
-        console.log(userchoise);
-        console.log(choixDelIa);
-        console.log("gagné");
-        document.getElementById("quiGagne").textContent = "Gooood !";
-    } else if (userchoise == "pa" & choixDelIa == "pi"){
-        console.log(userchoise);
-        console.log(choixDelIa);
-        console.log("gagné");
-        document.getElementById("quiGagne").textContent = "Chapeau !!!";
-    } else if (userchoise == "ci" & choixDelIa == "pa"){
-        console.log(userchoise);
-        console.log(choixDelIa);
-        console.log("gagné");
-        document.getElementById("quiGagne").textContent = "Bravissimo !!!";
-    } else{
-        console.log(userchoise);
-        console.log(choixDelIa);
-        console.log("perdu");
-        document.getElementById("quiGagne").textContent = "Oh noooo ... Bowser gagne ...";
+/*choix de l'IA au hasard, avec changement de son image*/
+function getComputerChoice() {
+    const choices = ['pi', 'pa', 'ci'];
+    const randomNumber =  Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+
+    if (randomNumber === 0){
+        document.getElementById("imgIa").src = "style/img/pierre.png";
+    } else if (randomNumber === 1){
+        document.getElementById("imgIa").src = "style/img/feuille.png";
+    } else if (randomNumber === 2){
+        document.getElementById("imgIa").src = "style/img/ciseaux.png";
+    }
+    return choices[randomNumber];
+}
+
+
+/*fonction jeu - comparation des choix joueurs et IA */
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "pici":
+        case "papi":
+        case "cipa":
+            console.log("choix du joueur => " + userChoice);
+            console.log("choix de l'ordi => " + computerChoice);
+            document.getElementById("quiGagne").textContent = "Gooood !";
+        break;
+        case "pipa":
+        case "paci":
+        case "cipi":
+            console.log("choix du joueur => " + userChoice);
+            console.log("choix de l'ordi => " + computerChoice);
+            document.getElementById("quiGagne").textContent = "Bowser gagne !!";
+        break;
+        case "pipi":
+        case "papa":
+        case "cici":
+            console.log("choix du joueur => " + userChoice);
+            console.log("choix de l'ordi => " + computerChoice);
+            document.getElementById("quiGagne").textContent = "Match nul ...";
+        break;
     }
 }
 
-function imageIa (){
-    if (choixDelIa === "pi"){
-        document.getElementById(imgIa).src = url("img/pierre.png");
-    }
-}
+main();
+
 
 /*rafraichir la page*/
 function reload() 
